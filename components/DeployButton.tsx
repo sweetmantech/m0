41cc6165baf81777fda62f1fc7feb39c9434d3bc
Sweets Sweetman <sweetmantech@gmail.com>
2025-05-30 17:32:48 -0300

    show deploy button on mobile

--- components/DeployButton.tsx
+++ components/DeployButton.tsx
@@ -11,7 +11,7 @@ export default function DeployButton() {
   return (
     <>
       <Button
-        className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
+        className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
         onClick={() => setOpen((prev) => !prev)}
       >
         <VercelIcon size={16} />
